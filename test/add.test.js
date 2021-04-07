5e6257d5102151a0b4b9bcdfd7c2ba8acc5b6019
liang <2275164048@qq.com>
2021-04-07 17:40:07 +0800

    ok

--- test/add.test.js
+++ test/add.test.js
@@ -1,6 +1,6 @@
 const add = require('../add.js'),
       expect = require('chai').expect;
-
+/*
 describe('加法函数的单元测试',()=>{
   //测试用例
   it('0+0=0',()=>{
@@ -10,3 +10,23 @@ describe('加法函数的单元测试',()=>{
     expect(isNaN(add(1,'a'))).to.be.ok;
   });
 })
+*/
+
+describe('加法函数的动态测试',()=>{
+ let data=[{
+    input:[0,0],
+    output:0
+  },{
+    input:[1,1],
+    output:2
+  },{
+    input:[1,2e3],
+    output:NaN
+  }];
+  data.forEach((item)=>{
+    it(`${item.input[0]}+${item.input[1]}= ${item.output}`,()=>{
+      expect(add(item.input[0],item.input[1])).to.be.equal(item.output);
+    });
+  });
+});
+
