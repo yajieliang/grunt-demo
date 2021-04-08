cec78fcaf518e376f21487f2b1b7d1d6895d9fb0
liang <2275164048@qq.com>
2021-04-08 10:28:40 +0800

    uglify

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+  // 具体构建任务的配置信息，主要包括：源文件、目标文件和配置参数
+  grunt.initConfig({
+    uglify: {
+     my_target: {
+      files: {
+        'dist/jquery.min.js': ['jquery.js']
+      } 
+    }
+  }
+  });
+
+  // 加载构建任务插件，每行加载一个插件，需要几个插件，写几行
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // 定义构建任务清单，可以有多个构建任务清单
+  grunt.registerTask('default', ['uglify']);
+};
