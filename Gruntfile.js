2f3b47d1e25038767d7a3071282fb3aa0510970e
liang <2275164048@qq.com>
2021-04-09 16:14:31 +0800

    imagemin

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+
+module.exports = function (grunt) {
+  // 具体构建任务的配置信息，主要包括：源文件、目标文件和配置参数
+  grunt.initConfig({
+    imagemin:{
+      dynamic: {
+        files: [{    
+          expand: true,
+          src: ['images/*.{png,jpg,gif}'],
+          dest: 'dist/'
+        }] 
+    } 
+  }
+  });
+
+  // 加载构建任务插件，每行加载一个插件，需要几个插件，写几行
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+  // 定义构建任务清单，可以有多个构建任务清单
+  grunt.registerTask('default', ['imagemin']);
+};
+
